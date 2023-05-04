5487162dae40c3d62bc1ec2538bcba58e3813dfd
francescocabras <cabras181@gmail.com>
2023-05-04 14:43:43 +0200

    player

--- src/main/kotlin/Player.kt
+++ src/main/kotlin/Player.kt
@@ -1,2 +1,22 @@
-class Player {
+class Player(val age: Int, val name: String, val profession: String) {
+
+
+
+    fun introduce() {
+        println("Hallo...")
+        Thread.sleep(3000)
+        println("mein Name ist $name")
+        Thread.sleep(3000)
+        println("...Wo zum Teufel sind wir.....God dam....")
+        Thread.sleep(3000)
+        println("Unknow girl:...hallo John....es ist so Dunkel hier..und ich kann mich nicht bewegen..")
+        Thread.sleep(3000)
+        println("Ich sehe nach ob ich eine Lösung finde...")
+        Thread.sleep(6000)
+
+
+
+
+
+    }
 }
